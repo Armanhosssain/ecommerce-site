15770b9070c28c8a25d20829987c7eddffb529c5
Arman hossain <armanh686@gmail.com>
2019-09-02 09:35:17 +0600

    added all

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// second slider
+$(document).ready(function(){
+$(".second-slider").owlCarousel({
+items:3,
+loop:true,
+dots:true,
+dotsEach:true,
+autoplay:true,
+});
+});
+// first slider
+$(document).ready(function(){
+$(".owl-carousel").owlCarousel({
+items:1,
+loop:true,
+dots:true,
+dotsEach:true,
+autoplay:true,
+});
+});
